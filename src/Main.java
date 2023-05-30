47cf30355e9080028eabc0fd9f74f0b6ef3ecb06
naryn <narynbeknurbekov@gmail.com>
2023-05-30 23:13:38 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        User user = new User(1, "Naryn", 25, Gender.FEMALE);
-        User user1 = new User(2, "Narynbek", 26, Gender.MALE);
-        User user2 = new User(3, "Narynbek1", 25, Gender.FEMALE);
+        User user = new User(1, "N", 25, Gender.FEMALE);
+        User user1 = new User(2, "Y", 26, Gender.MALE);
+        User user2 = new User(3, "Z", 25, Gender.FEMALE);
 
         UserService userService = new UserServiceImpl();
         userService.addUser(user);
@@ -20,14 +20,16 @@ public class Main {
         userService.addUser(user2);
         System.out.println(userService.ollUsers());
 
+        ArrayList<User> users = new ArrayList<>();
+        User[] users1 = new User[]{user, user1, user2};
+        UserDao userDao = new UserDao();
+
+
         try {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        ArrayList<User> users = new ArrayList<>();
-        User[] users1 = new User[]{user, user1, user2};
-        UserDao userDao = new UserDao();
 
     }
 }
